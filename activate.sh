c7664a0293962279e6468411c294b75e6963909c
Hamish Ivison <hamishivi@gmail.com>
2018-08-13 14:10:35 +1000

    add activate script

---
+++ activate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# we will be using virtualenv because the latest version of python (3.7) is not supported (yet) by tensorflow!
+source env/bin/activate
+
+# run this script as ". ./activate.sh"
