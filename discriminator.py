26365e6e9facb5c547ce6bf9b606f02674f5d423
Hamish Ivison <hamishivi@gmail.com>
2018-08-17 00:04:15 +1000

    move back to crossentropy

--- discriminator.py
+++ discriminator.py
@@ -1,5 +1,5 @@
 import keras
-from keras.models import Sequential
+from keras.models import Sequential, Model
 import keras.backend as K
 from keras.layers import Conv2D, Dense, Flatten, BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
@@ -14,25 +14,31 @@ def make_discriminator(input_shape):
     model = Sequential()
 
     model.add(Conv2D(32, kernel_size=(5, 5), input_shape=input_shape, padding='same'))
-    model.add(BatchNormalization())
     model.add(LeakyReLU())
 
     model.add(Conv2D(64, kernel_size=(5, 5)))
-    model.add(BatchNormalization())
     model.add(LeakyReLU())
 
-    model.add(Conv2D(64, kernel_size=(5, 5)))
-    model.add(BatchNormalization())
+    model.add(Conv2D(128, kernel_size=(5, 5)))
     model.add(LeakyReLU())
 
     model.add(Flatten())
-    model.add(Dense(1024))
-    model.add(LeakyReLU())
-    model.add(Dense(1))
+
+    if __name__ == "__main__":
+        model.add(Dense(10, activation='softmax'))
+    else:
+        model.add(Dense(1, activation='linear'))
     return model
 
 def compile_wasserstein_critic(model):
-    model.compile(loss=EM_loss, optimizer=keras.optimizers.RMSprop(lr=0.00005, clipvalue=0.01), metrics=["accuracy"])
+    model.compile(loss=EM_loss,
+        optimizer=keras.optimizers.RMSprop(lr=0.00005, clipvalue=0.01),
+        metrics=["accuracy"])
+
+def compile_demo(model):
+    model.compile(loss='sparse_categorical_crossentropy',
+        optimizer=keras.optimizers.RMSprop(lr=0.00005, clipvalue=0.01),
+        metrics=["accuracy"])
 
 # Example: mnist data set (digit recognition)
 if __name__ == "__main__":
@@ -40,13 +46,13 @@ if __name__ == "__main__":
     import sys
 
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    y_train = [1 if y > 4 else -1 for y in y_train]
-    y_test = [1 if y > 4 else -1 for y in y_test]
     x_train = (x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32')  - 127.5) / 127.5
     x_test = (x_test.reshape(x_test.shape[0], 28, 28, 1).astype('float32')  - 127.5) / 127.5
+    y_test = keras.utils.to_categorical(y_test, num_classes=10)
+    y_train = keras.utils.to_categorical(y_train, num_classes=10)
     model = make_discriminator((28, 28, 1))
     # no EM as that needs to be paired with the generator
-    compile_wasserstein_critic(model)
+    compile_demo(model)
     model.fit(x_train, y_train,
           batch_size=128,
           epochs=6,
