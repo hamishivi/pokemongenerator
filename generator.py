c15600271b0d79a09e63ccba6199a02170f3ce31
Hamish Ivison <hamishivi@gmail.com>
2018-09-04 11:07:22 +1000

    reshape generator

--- generator.py
+++ generator.py
@@ -7,18 +7,30 @@ from discriminator import make_discriminator
 from data_prep import prepare_images
 from PIL import Image
 
-def make_generator():
+def make_generator(input_shape=(100,)):
     model = Sequential()
+    if __name__ == "__main__":
+        model.add(Conv2D(2, kernel_size=(5, 5), strides=[4,4], input_shape=input_shape, data_format="channels_last", padding='same'))
+        model.add(LeakyReLU())
 
-    # takes in 100dim noise vector as seed
-    model.add(Dense(4 * 4 * 512, input_dim=100))
+        model.add(Conv2D(64, kernel_size=(5, 5), strides=[2,2], input_shape=input_shape, data_format="channels_last", padding='same'))
+        model.add(LeakyReLU())
+
+        model.add(Conv2D(128, kernel_size=(5, 5), strides=[2,2], input_shape=input_shape, data_format="channels_last", padding='same'))
+        model.add(LeakyReLU())
+
+    else:
+        # takes in 100dim noise vector as seed
+        model.add(Dense(4 * 4 * 512, input_dim=100))
     model.add(LeakyReLU())
+
+    model.summary()
 
     model.add(Reshape((4, 4, 512)))
     model.add(BatchNormalization())
     model.add(LeakyReLU())
 
-    model.add(Conv2DTranspose(256, kernel_size=(5, 5), strides=[2,2], padding='same'))
+    model.add(Conv2DTranspose(256, kernel_size=(5, 5), strides=[4,4], padding='same'))
     model.add(BatchNormalization())
     model.add(LeakyReLU())
 
@@ -26,22 +38,14 @@ def make_generator():
     model.add(BatchNormalization())
     model.add(LeakyReLU())
 
-    model.add(Conv2DTranspose(64, kernel_size=(5, 5), strides=[2,2], padding='same'))
-    model.add(BatchNormalization())
-    model.add(LeakyReLU())
-
-    model.add(Conv2DTranspose(32, kernel_size=(5, 5), strides=[2,2], padding='same'))
-    model.add(BatchNormalization())
-    model.add(LeakyReLU())
-
-    model.add(Conv2DTranspose(16, kernel_size=(5, 5), strides=[2,2], padding='same'))
+    model.add(Conv2DTranspose(64, kernel_size=(5, 5), strides=[4,4], padding='same'))
     model.add(BatchNormalization())
     model.add(LeakyReLU())
 
     model.add(Conv2DTranspose(3, kernel_size=(5, 5), strides=[2,2], padding='same', data_format='channels_last'))
     model.add(Activation("tanh"))
 
-    noise = Input(shape=(100,))
+    noise = Input(shape=input_shape)
     
     img = model(noise)
 
@@ -64,9 +68,10 @@ def compile_demo(deconv, conv):
     return combined
 
 if __name__ == '__main__':
-    deconv_layers = make_generator()
-    conv_layers = make_discriminator((128, 128, 3), generator_demo=True)
-    model = compile_demo(deconv_layers, conv_layers)
+    deconv_layers = make_generator(input_shape=(128,128,3))
+    deconv_layers.compile(loss=keras.losses.categorical_crossentropy,
+        optimizer=keras.optimizers.Adam(lr=1e-5),
+        metrics=["accuracy"])
     batch_size = 64
 
     image_datagen = prepare_images("Semantic_dataset100/image", batch_size, (128, 128), shuffle=False, class_mode=None)
@@ -76,10 +81,10 @@ if __name__ == '__main__':
 
     train_datagen = zip(image_datagen, mask_datagen)
 
-    model.fit_generator(train_datagen, steps_per_epoch=2000, epochs=5)
+    deconv_layers.fit_generator(train_datagen, steps_per_epoch=2000, epochs=5)
 
     print("Showing example segmentation...")
-    results = model.predict_generator(test_datagen,30,verbose=1)
+    results = deconv_layers.predict_generator(test_datagen,30,verbose=1)
     for idx, image in enumerate(results):
         im = Image.fromarray(image)
         im.save("result_." + idx + "png")
