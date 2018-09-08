a9d118162f3e2f6f32e854c4841d9101ce8bdbfa
Hamish Ivison <hamishivi@gmail.com>
2018-09-08 16:35:33 +1000

    edit wgan and generator for new stuff

--- generator.py
+++ generator.py
@@ -3,7 +3,8 @@ import keras
 from keras.models import Sequential, Model
 import keras.backend as K
 from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import Activation, Conv2D, Conv2DTranspose, Dense, Flatten, BatchNormalization, Reshape, Input
+from keras.layers import Activation, Conv2D, Conv2DTranspose, Dense, Flatten, BatchNormalization, Reshape, Input, MaxPooling2D
+
 from keras.layers.advanced_activations import LeakyReLU
 from discriminator import make_discriminator
 from data_prep import prepare_images
@@ -12,15 +13,17 @@ from scipy.misc import imsave, imread
 def make_generator(input_shape=(100,)):
     model = Sequential()
     if __name__ == "__main__":
-        model.add(Conv2D(2, kernel_size=(5, 5), strides=[4,4], input_shape=input_shape, data_format="channels_last", padding='same'))
+        model.add(Conv2D(2, kernel_size=(5, 5), input_shape=input_shape, data_format="channels_last", padding='same'))
         model.add(LeakyReLU())
+        model.add(MaxPooling2D((2, 2), padding='same'))
 
         model.add(Conv2D(64, kernel_size=(5, 5), strides=[2,2], input_shape=input_shape, data_format="channels_last", padding='same'))
         model.add(LeakyReLU())
+        model.add(MaxPooling2D( MaxPooling2D((2, 2), padding='same')))
 
         model.add(Conv2D(128, kernel_size=(5, 5), strides=[2,2], input_shape=input_shape, data_format="channels_last", padding='same'))
         model.add(LeakyReLU())
-
+        model.add(MaxPooling2D())
     else:
         # takes in 100dim noise vector as seed
         model.add(Dense(4 * 4 * 512, input_dim=100))
@@ -32,19 +35,23 @@ def make_generator(input_shape=(100,)):
     model.add(BatchNormalization())
     model.add(LeakyReLU())
 
-    model.add(Conv2DTranspose(256, kernel_size=(5, 5), strides=[4,4], padding='same'))
+    model.add(Conv2DTranspose(256, kernel_size=(5, 5), strides=[2, 2], padding='same'))
     model.add(BatchNormalization())
     model.add(LeakyReLU())
 
-    model.add(Conv2D(128, kernel_size=(5, 5), padding='same'))
+    model.add(Conv2DTranspose(128, kernel_size=(5, 5), strides=[2, 2], padding='same'))
     model.add(BatchNormalization())
     model.add(LeakyReLU())
 
-    model.add(Conv2DTranspose(64, kernel_size=(5, 5), strides=[4,4], padding='same'))
+    model.add(Conv2DTranspose(64, kernel_size=(5, 5), strides=[2, 2], padding='same'))
     model.add(BatchNormalization())
     model.add(LeakyReLU())
 
-    model.add(Conv2DTranspose(3, kernel_size=(5, 5), strides=[2,2], padding='same', data_format='channels_last'))
+    model.add(Conv2DTranspose(32, kernel_size=(5, 5), strides=[2, 2], padding='same'))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+
+    model.add(Conv2DTranspose(3, kernel_size=(5, 5), strides=[2, 2], padding='same', data_format='channels_last'))
     model.add(Activation("tanh"))
 
     noise = Input(shape=input_shape)
@@ -114,6 +121,7 @@ if __name__ == '__main__':
     test_datagen = get_demo_test()
     print("Showing example segmentation...")
     image = imread('segmentation_dataset/images/test/abbey/ADE_val_00000001.jpg')
+    image = imresize(image, (128, 128))
     image = np.expand_dims(image, axis=0)
     print(image.shape)
     results = deconv_layers.predict(image, test_datagen, verbose=1)
